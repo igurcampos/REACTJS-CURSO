4613c70391c8c7509c230501e7e167f9028b26a6
Igor <igorvinicius.only@gmail.com>
2021-09-29 20:07:35 -0300

    sobre typescript

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,13 @@ module.exports = {
 
     devtool: isDevelement ? 'eval-source-map': 'source-map',
 
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
 
     devServer: {
@@ -32,7 +32,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader:'babel-loader',
